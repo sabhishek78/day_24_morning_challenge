13e749e5b85fd9e1bb9779dcb7fe8972aa4ea6ee
abhishek shrivastava <abhishekshrivastava78@gmail.com>
2019-11-14 14:08:00 +0530

    code refactored

--- bin/main.dart
+++ bin/main.dart
@@ -34,44 +34,26 @@ bool checkPanDigital(int input){
 //  ) ➞ 6
 
 
-int overlappingRectangles(List<Map> Rect1, List<Map> Rect2){
-
-List<int> x=[];
-List<int> y=[];
-x.add(Rect1[0]['x']);
-x.add(Rect1[1]['x']);
-x.add(Rect2[0]['x']);
-x.add(Rect2[1]['x']);
-y.add(Rect1[0]['y']);
-y.add(Rect1[1]['y']);
-y.add(Rect2[0]['y']);
-y.add(Rect2[1]['y']);
-if(overlap(Rect1[1]['x'],Rect2[0]['x'],Rect1[1]['y'],Rect2[0]['y'])){
-  x.sort();
-  y.sort();
-  return ((x[1]-x[2]).abs())*((y[1]-y[2]).abs());
+int overlappingRectangles(List<Map> Rect1, List<Map> Rect2) {
+  List<int> r1x = [];
+  List<int> r2x = [];
+  List<int> r1y = [];
+  List<int> r2y = [];
+  r1x.addAll(Rect1.map((item) => item['x']));
+  r2x.addAll(Rect2.map((item) => item['x']));
+  r1y.addAll(Rect1.map((item) => item['y']));
+  r2y.addAll(Rect2.map((item) => item['y']));
+  r1x.sort();
+  r1y.sort();
+  r2x.sort();
+  r2y.sort();
+  return (r1x[1] > r2x[0] || r1y[1] > r2y[0])?  ((r1x[1] - r2x[0])) * ((r1y[1] - r2y[0])):  0;
 }
-else {
-  return 0;
-}
-
-
-}
-bool overlap(int x2, int x3, int y2 , int y3){
-  if(x2>x3 || y2>y3){
-    return true;
-  }
-  else {
-    return false;
-  }
-
-}
-
 main() {
 //print(checkPanDigital(11234567890));
 //print(checkPanDigital(125));
 print(overlappingRectangles([{ 'x': 5, 'y': 0 }, { 'x': 0, 'y': 10 }],[{ 'x': 15, 'y': 15 }, { 'x': 20, 'y': 20 }]));
 print(overlappingRectangles([{ 'x': 2, 'y': 1 }, { 'x': 5, 'y': 5 }],[{ 'x': 3, 'y': 2 }, { 'x': 5, 'y': 7 }]));
-
+print(overlappingRectangles([{ 'x': 1, 'y': 0 }, { 'x': 2, 'y': 5 }],[{ 'x': 2, 'y': 0 }, { 'x': 3, 'y': 5 }]));
 }
 
